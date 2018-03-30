c9fa5a3f9b27e3feb4197ae3954f96efff980847
Bodo Kaiser <i@bodokaiser.io>
2018-03-30 11:22:36 +0200

    moved server.go to cmd package

--- cmd/httpd/main.go
+++ cmd/httpd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
 	"github.com/bodokaiser/beagle/httpd"
 )
 
+type config struct {
+	address string
+}
+
 func main() {
+	c := config{}
+
+	flag.StringVar(&c.address, "address", ":8000", "")
+	flag.Parse()
+
 	e := echo.New()
 	e.Renderer = httpd.NewTemplate("views/*.html")
 
@@ -15,9 +26,10 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", httpd.IndexHandler)
+	e.PATCH("/", httpd.PatchHandler)
 
 	e.Static("/stylesheets", "public/stylesheets")
 	e.Static("/javascripts", "public/javascripts")
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(c.address))
 }
