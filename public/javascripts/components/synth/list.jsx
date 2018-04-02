4d78897d1eae04adb5a33c1efc70cb14c8fde66d
Bodo Kaiser <i@bodokaiser.io>
2018-04-02 12:36:07 +0200

    added preact router

---
+++ public/javascripts/components/synth/list.jsx
@@ -0,0 +1,8 @@
+import {h, Component} from 'preact'
+
+
+export default class SynthList extends Component {
+  render() {
+    return <ul><li>Synth1</li><li>Synth2</li></ul>
+  }
+}
--- public/javascripts/index.jsx
+++ public/javascripts/index.jsx
@@ -1,9 +1,16 @@
 import {h, render} from 'preact'
+import Router from 'preact-router'
 
 import App from './components/app'
+import SynthList from './components/synth/list'
 
 let content = document.querySelector('content')
 
-render(<App /> , content, content.firstElementChild)
+render(
+  <Router>
+    <App path="/" />
+    <SynthList path="/synth" /> 
+  </Router>,
+  content, content.firstElementChild)
 
 if (module.hot) module.hot.accept()
