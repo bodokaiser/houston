7633dee76866ba76d74c7dc9540a3faf9da68944
Bodo Kaiser <i@bodokaiser.io>
2018-04-08 11:30:00 +0200

    updated cmd/cli2/main

--- cmd/cli2/main.go
+++ cmd/cli2/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"log"
-	"time"
 
-	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/beaglebone"
 )
 
 func main() {
 	beagle := beaglebone.NewAdaptor()
 
-	led := gpio.NewLedDriver(beagle, "P8_18")
-	if err := led.Start(); err != nil {
+	c, err := beagle.GetSpiConnection(2, 5e6, 0)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	led.On()
-	time.Sleep(time.Millisecond)
-	led.Off()
+	w := []byte{0x00}
+	r := []byte{0x00}
+
+	err = c.Tx(w, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
