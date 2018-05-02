70bb984d8b2be2ab471a7e44d6ff078ccafbf1fb
Bodo Kaiser <i@bodokaiser.io>
2018-05-02 14:57:14 +0200

    moved web app to extra repository

--- app/javascripts/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux'
-
-import devicesReducers from './devices'
-import systemReducers from './system'
-
-export default combineReducers({
-  devices: devicesReducers,
-  system: systemReducers
-})
--- app/javascripts/reducers/system.js
+++
@@ -1,8 +0,0 @@
-import {createReducer} from 'redux-create-reducer'
-
-export default createReducer({
-  cpu: { name: 'CPU Usage', value: '50%' },
-  ram: { name: 'RAM Usage', value: '60%' },
-  disk: { name: 'Disk Usage', value: '30%' },
-  uptime: { name: 'Uptime', value: '30h' }
-}, {})
